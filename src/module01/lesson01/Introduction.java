54985c97ffc7aa76da7035352e4cd30a170f529a
marina2302 <marina23021995@gmail.com>
2020-07-10 16:14:32 +0300

    fix2 lesson1

--- src/module01/lesson01/Introduction.java
+++ src/module01/lesson01/Introduction.java
@@ -5,6 +5,7 @@ public class Introduction {
         for (int i = 0; i < 5; i++) {
             System.out.print("Hello! My name is Marina. ");
         }
+        System.out.println();
         for (int i = 0; i < 5; i++) {
             System.out.println("I want to be a java developer!");
         }
