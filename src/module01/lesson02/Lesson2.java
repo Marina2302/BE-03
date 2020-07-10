3bdcd9f9d38f1ffc1b1c1035ea45dc8a99d8fdfc
marina2302 <marina23021995@gmail.com>
2020-07-10 15:53:01 +0300

    fix commit 6

--- src/module01/lesson02/Lesson2.java
+++ src/module01/lesson02/Lesson2.java
@@ -4,5 +4,7 @@ public class Lesson2 {
     public void changes(){
         System.out.println("Changes for commit 1");
         System.out.println("Changes for commit 2");
+        System.out.println("Changes for commit 3/check merge conflicts");
+        System.out.println("Changes for commit 4");
     }
 }
