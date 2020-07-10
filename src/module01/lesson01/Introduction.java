0e3cac1b055452a6fe39f29da7781ae9685f2408
marina2302 <marina23021995@gmail.com>
2020-07-10 16:08:14 +0300

    fix lesson1

--- src/module01/lesson01/Introduction.java
+++ src/module01/lesson01/Introduction.java
@@ -3,7 +3,9 @@ package module01.lesson01;
 public class Introduction {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            System.out.println("Hello! My name is Marina.");
+            System.out.print("Hello! My name is Marina. ");
+        }
+        for (int i = 0; i < 5; i++) {
             System.out.println("I want to be a java developer!");
         }
     }
