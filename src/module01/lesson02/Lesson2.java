e7ecf2276eac706fb7f235f35b436b11a1896133
marina2302 <marina23021995@gmail.com>
2020-07-10 15:52:49 +0300

    fix commit 5

--- src/module01/lesson02/Lesson2.java
+++ src/module01/lesson02/Lesson2.java
@@ -3,5 +3,6 @@ package module01.lesson02;
 public class Lesson2 {
     public void changes(){
         System.out.println("Changes for commit 1");
+        System.out.println("Changes for commit 2");
     }
 }
