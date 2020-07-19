e54cc7afde2e2f0412475ec40075785c6b8bf4ac
marina2302 <marina23021995@gmail.com>
2020-07-19 14:57:49 +0300

    lesson4 task46

---
+++ src/module01/lesson04/ProgramTask46.java
@@ -0,0 +1,15 @@
+package module01.lesson04;
+
+public class ProgramTask46 {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 3;
+        System.out.println(a & b);
+        System.out.println(a | b);
+        System.out.println(~a);
+        System.out.println(a ^ b);
+
+        // The bitwise operators are like logical operators
+        // After applying the NOT (~) operator, the number will change sign
+    }
+}
