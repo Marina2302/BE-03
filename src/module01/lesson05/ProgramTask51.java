cc6816b3b46e57152de4d367d6191fa19a9b768d
marina2302 <marina23021995@gmail.com>
2020-07-20 22:10:48 +0300

    lesson5 task51

---
+++ src/module01/lesson05/ProgramTask51.java
@@ -0,0 +1,13 @@
+package module01.lesson05;
+
+public class ProgramTask51 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = --a;
+
+        if (b == 7) {
+            System.out.println("b = 7");
+        }
+        // Nothing will be displayed on the console as a result
+    }
+}
