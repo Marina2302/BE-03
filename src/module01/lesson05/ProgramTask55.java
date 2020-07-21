96b0289cd6e13206213ab718fb3d4613fd8c558a
marina2302 <marina23021995@gmail.com>
2020-07-21 19:58:49 +0300

    lesson5 task55

---
+++ src/module01/lesson05/ProgramTask55.java
@@ -0,0 +1,9 @@
+package module01.lesson05;
+
+public class ProgramTask55 {
+    public static void main(String[] args) {
+        int num = 8;
+        String message = num == 8 ? "Number is less than or equal to 10" : "Number is greater than 10";
+        System.out.println(message);
+    }
+}
