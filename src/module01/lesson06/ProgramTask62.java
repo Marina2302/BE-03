04ac502fc7dfb474c514e306a2ed295bf0089097
marina2302 <marina23021995@gmail.com>
2020-07-23 22:14:35 +0300

    lesson6 task62

---
+++ src/module01/lesson06/ProgramTask62.java
@@ -0,0 +1,17 @@
+package module01.lesson06;
+
+import java.util.Arrays;
+
+public class ProgramTask62 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] reverse = new int[10];
+        int n = array.length;
+
+        for (int i = n - 1, j = 0; i >= 0; i--, j++) {
+            reverse[j] = array[i];
+        }
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(reverse));
+    }
+}
