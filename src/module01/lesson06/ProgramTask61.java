e5ef15b38a9f28b23efb2bd28d4f5b1137617601
marina2302 <marina23021995@gmail.com>
2020-07-23 22:02:16 +0300

    lesson6 task61

---
+++ src/module01/lesson06/ProgramTask61.java
@@ -0,0 +1,10 @@
+package module01.lesson06;
+
+import java.util.Arrays;
+
+public class ProgramTask61 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(Arrays.toString(array));
+    }
+}
