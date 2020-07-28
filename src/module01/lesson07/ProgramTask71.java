89fb9934c524f76a6544c4af066a0b9bdefcb8e1
marina2302 <marina23021995@gmail.com>
2020-07-28 21:30:06 +0300

    lesson7 task71

---
+++ src/module01/lesson07/ProgramTask71.java
@@ -0,0 +1,11 @@
+package module01.lesson07;
+
+public class ProgramTask71 {
+    public static void main(String[] args) {
+        for (int i = 10; i <= 20; i++) {
+            int j;
+            j = i * i;
+            System.out.println(j);
+        }
+    }
+}
