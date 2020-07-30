da916416219066793286d2b9f32d71513d708864
marina2302 <marina23021995@gmail.com>
2020-07-30 23:57:37 +0300

    lesson8 task81.2

--- src/module01/lesson08/ProgramTask81.java
+++ src/module01/lesson08/ProgramTask81.java
@@ -3,6 +3,11 @@ package module01.lesson08;
 public class ProgramTask81 {
     public static void main(String[] args) {
         display();
+        System.out.println("----------");
+        sum(2);
+        System.out.println("----------");
+
+
     }
 
     /**
@@ -13,5 +18,17 @@ public class ProgramTask81 {
         for (int i = 1; i <= 20; i++) {
             System.out.print(i + " " + ++i + "\n");
         }
+    }
+
+    /**
+     * Write a method with one parameter that will output the sum of numbers from 1 to the given parameter.
+     * Call it in the main method.
+     */
+    public static void sum(int num) {
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
     }
 }
