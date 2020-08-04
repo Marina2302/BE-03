92233d205a32ae7f52d31f95603b4f086c25f5dc
marina2302 <marina23021995@gmail.com>
2020-08-04 20:39:18 +0300

    lesson9 task1

---
+++ src/module01/lesson09/ProgramTask91.java
@@ -0,0 +1,17 @@
+package module01.lesson09;
+
+public class ProgramTask91 {
+    public static void main(String[] args) {
+        /**
+         * How many objects will be in a String Pool?
+         *
+         * String st = "Java";
+         * String st2 = new String("Java");
+         * String st3 = st2.intern();
+         * String st4 = "java";
+         * String st = null;
+         * String st6 = new String("JAVA");
+         */
+        //5
+    }
+}
