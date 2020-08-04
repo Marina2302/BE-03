c95a964262b4890caad847bfb1bee61a223b714e
marina2302 <marina23021995@gmail.com>
2020-08-04 20:39:37 +0300

    lesson09 task2

---
+++ src/module01/lesson09/ProgramTask92.java
@@ -0,0 +1,13 @@
+package module01.lesson09;
+
+public class ProgramTask92 {
+    public static void main(String[] args) {
+        /**
+         * You have a phrase "London - is a capital of England".
+         * You need to change this phrase to "London - is a capital of Great Britain".
+         */
+        String s = "London - is a capital of England";
+        System.out.println(s.replace("England", "Great Britain"));
+
+    }
+}
