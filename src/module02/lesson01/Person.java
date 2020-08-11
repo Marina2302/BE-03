3538a6b33968438e11523365978569aed5b25845
marina2302 <marina23021995@gmail.com>
2020-08-11 23:39:12 +0300

    lesson01 task2

--- src/module02/lesson01/Person.java
+++ src/module02/lesson01/Person.java
@@ -1,15 +1,38 @@
 package module02.lesson01;
 
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
 public class Person {
     String name;
     int age;
 
-    public Person(String name, int age){
+    public Person(){
 
+    }
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public void meditation(){
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EE MMM dd HH:mm:ss yyyy");
+        String formatDateTime = now.format(formatter);
+        System.out.println("I think - it means I exist. (" + this.name + ") " + formatDateTime);
     }
 
     public static void main(String[] args) {
         Person person = new Person("Sam", 33);
+        person.meditation();
     }
 
 }
