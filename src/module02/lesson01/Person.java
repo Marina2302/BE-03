9d777f0aaca785f1eeeaf17384241e5b8b8fe492
marina2302 <marina23021995@gmail.com>
2020-08-11 23:07:05 +0300

    lesson01 task1

---
+++ src/module02/lesson01/Person.java
@@ -0,0 +1,15 @@
+package module02.lesson01;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age){
+
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person("Sam", 33);
+    }
+
+}
