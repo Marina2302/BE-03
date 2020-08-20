52c1200b72501f443b14c1dc4f37d451b6d27e68
marina2302 <marina23021995@gmail.com>
2020-08-20 22:25:18 +0300

    lesson04 task2

--- src/module02/lesson04/AnotherClass.java
+++ src/module02/lesson04/AnotherClass.java
@@ -1,11 +1,21 @@
 package module02.lesson04;
 
 public class AnotherClass {
+
+    private static int count;
+
     public static void main(String[] args) {
         ClassWithPrivateVariables cwpv = new ClassWithPrivateVariables();
         cwpv.setName("Some");
         cwpv.setAge(5);
         cwpv.getName();
         cwpv.getAge();
+        System.out.println();
+
+        for (int i = 0; i < 3; i ++){
+            System.out.println(new ClassWithPrivateVariables());
+            count++;
+        }
+        System.out.println(count);
     }
 }
