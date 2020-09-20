8f43eb0c3f5dfe615a7dd123a38b267bd17cb838
marina2302 <marina23021995@gmail.com>
2020-09-20 15:37:48 +0300

    lesson12 task2

--- src/module02/lesson12/ParameterizedClass.java
+++ src/module02/lesson12/ParameterizedClass.java
@@ -1,5 +1,7 @@
 package module02.lesson12;
 
+import java.util.List;
+
 public class ParameterizedClass<T, V, K> {
     private T st;
     private V in;
@@ -7,5 +9,13 @@ public class ParameterizedClass<T, V, K> {
 
     public K get(){
         return bl;
+    }
+
+    public List<String> getDefaultList(List<String> list){
+        list.add("hello");
+        list.add("hello");
+        list.add("hello");
+        list.add("hello");
+        return list;
     }
 }
