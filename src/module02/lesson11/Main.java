5cc618bd259b0dff1e41e7e92ab5bf7e9b7d4215
marina2302 <marina23021995@gmail.com>
2020-09-20 14:51:13 +0300

    lesson11 task3-5

--- src/module02/lesson11/Main.java
+++ src/module02/lesson11/Main.java
@@ -1,10 +1,52 @@
 package module02.lesson11;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
         Person person = new Person();
         person.setName("Mike");
         person.setCompany("Company");
+        String name = null;
+
+        try {
+            Field fieldAge = Person.class.getDeclaredField("age");
+            Field fieldName = Person.class.getDeclaredField("name");
+            fieldAge.setAccessible(true);
+            fieldName.setAccessible(true);
+            name = (String) fieldName.get(person);
+            fieldAge.set(person, 15);
+            Method method = person.getClass().getDeclaredMethod("getCompany");
+            method.setAccessible(true);
+            System.out.println(name + " " + person.getAge() + " " + method.invoke(person).toString());
+        } catch (NoSuchFieldException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(Arrays.toString(Person.class.getMethods()));
+        System.out.println("-----------------------------");
+        System.out.println(Arrays.toString(Person.class.getDeclaredMethods()));
+
+        System.out.println(Arrays.toString(Person.class.getInterfaces()));
+        System.out.println(Modifier.isPublic(Person.class.getModifiers()));
+        System.out.println(Modifier.isStatic(Person.class.getModifiers()));
+    }
+
+
+    private static Person getNewPerson() {
+        Class clazz;
+        Person person = null;
+        try {
+            clazz = Class.forName(Person.class.getName());
+            person = (Person) clazz.newInstance();
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException e) {
+            e.printStackTrace();
+        }
+        return person;
     }
 }
--- src/module02/lesson11/Person.java
+++ src/module02/lesson11/Person.java
@@ -1,9 +1,17 @@
 package module02.lesson11;
 
-public class Person {
+import java.io.Serializable;
+
+public class Person implements Serializable, Comparable {
     private String name;
     private int age;
     private String company;
+
+//    public Person(String name, int age, String company) {
+//        this.name = name;
+//        this.age = age;
+//        this.company = company;
+//    }
 
     public void setName(String name) {
         this.name = name;
@@ -19,5 +27,10 @@ public class Person {
 
     public void setCompany(String company) {
         this.company = company;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
     }
 }
