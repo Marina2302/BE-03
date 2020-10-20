a35089176c87391976c740d7d426845d6eecd4a5
marina2302 <marina23021995@gmail.com>
2020-10-20 16:30:24 +0300

    lesson3 task1

---
+++ src/module03/lesson03/Armstrong.java
@@ -0,0 +1,27 @@
+package module03.lesson03;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        long[] numbers = getNumbers(1000);
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    public static long[] getNumbers(long N) {
+        ArrayList<Long> list = new ArrayList<>();
+        for (long i = 1; i < N; i++) {
+            String s = String.valueOf(i);
+            double sum = 0;
+            for (int j = 0; j < s.length(); j++) {
+                sum = Math.pow(Integer.parseInt(s.substring(j, j + 1)), s.length()) + sum;
+            }
+            if(sum == i){
+                list.add(i);
+            }
+        }
+        long[] result = list.stream().mapToLong(l -> l).toArray();
+        return result;
+    }
+}
