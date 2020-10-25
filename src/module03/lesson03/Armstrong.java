4925da0ff61f0ff8804e21d9d3f695f67e674984
marina2302 <marina23021995@gmail.com>
2020-10-25 14:52:32 +0300

    lesson3 task1

--- src/module03/lesson03/Armstrong.java
+++ src/module03/lesson03/Armstrong.java
@@ -3,6 +3,18 @@ package module03.lesson03;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * Armstrong number = a natural number, which in a given number system is equal to the sum of its digits raised to a power equal to the number of its digits.
+ * S consists of M digits, for example S = 370 and M (number of digits) = 3.
+ * Implement the logic of the getNumbers method, which should be less than N (long) among natural numbers find all numbers that meet the following criteria:
+ * the number S is equal to the sum of its digits raised to the M power.
+ * getNumbers should return all such numbers in ascending order.
+ * Requirements:
+ * 1. The Solution class must have a public static long[] getNumbers (long N) method.
+ * 2. The getNumbers method should not throw any exceptions for any input.
+ * 3. All found numbers must be strictly less than N.
+ * 4. The getNumbers method should return an array of numbers satisfying the condition of the problem.
+ */
 public class Armstrong {
     public static void main(String[] args) {
        long[] numbers = getNumbers(1000);
