3da93722c580a81a6d2f40ca4754c587003b3fd3
marina2302 <marina23021995@gmail.com>
2020-10-28 19:25:02 +0300

    lesson5 task1

---
+++ src/module03/lesson05/ProgramTask51.java
@@ -0,0 +1,33 @@
+package module03.lesson05;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Create a set of lines, add 20 real words into it that start with the letter "H".
+ */
+public class ProgramTask51 {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("Home");
+        set.add("Harmonious");
+        set.add("Hypothesis");
+        set.add("Hype");
+        set.add("House");
+        set.add("Human");
+        set.add("Happy");
+        set.add("Hello");
+        set.add("Hard");
+        set.add("Harsh");
+        set.add("Hair");
+        set.add("Hand");
+        set.add("Hibernate");
+        set.add("History");
+        set.add("Hospital");
+        set.add("Heart");
+        set.add("Horticulturist");
+        set.add("Horizontal");
+        set.add("Headphones");
+        set.add("Highlight");
+    }
+}
