eec0b3dc6a14ec76e7732819e3f009e41ece3fde
marina2302 <marina23021995@gmail.com>
2020-10-29 22:40:48 +0300

    lesson5 task2

---
+++ src/module03/lesson05/ProgramTask52.java
@@ -0,0 +1,25 @@
+package module03.lesson05;
+
+import java.util.Scanner;
+import java.util.TreeSet;
+
+/**
+ * Enter from the console a string that contains only Latin letters, spaces, punctuation marks and dashes.
+ * Sort letters alphabetically and display the first 5 different letters on one line without separators.
+ * If the string contains less than 5 different letters, print them all.
+ * Letters of different case are considered the same.
+ * The case of the letters displayed does not affect the result.
+ * Use TreeSet.
+ * Example 1 input: zBk yaz b-kN
+ * Example 1 output: abkny
+ * Example 2 input: caAC A, aB? bB
+ * Example 2 output: abc
+ */
+public class ProgramTask52 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        TreeSet<Character> letters = new TreeSet<>();
+
+    }
+}
+
