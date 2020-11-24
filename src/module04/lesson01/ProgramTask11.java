f0ca02378174c48c7ff11d61bd82481ca3458361
marina2302 <marina23021995@gmail.com>
2020-11-24 21:38:57 +0300

    lesson01

---
+++ src/module04/lesson01/ProgramTask11.java
@@ -0,0 +1,18 @@
+package module04.lesson01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Change the behavior of "System.in". This command should get the information from the string or byte array, not from the console.
+ */
+public class ProgramTask11 {
+    public static void main(String[] args)
+            throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while ((s = in.readLine()).length() != 0)
+            System.out.println(s);
+    }
+}
