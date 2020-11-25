a68a8177fee043e1f2d6f66a11347b842edf89dd
marina2302 <marina23021995@gmail.com>
2020-11-25 23:57:10 +0300

    lesson01 task2

---
+++ src/module04/lesson01/ProgramTask12.java
@@ -0,0 +1,19 @@
+package module04.lesson01;
+
+import java.util.Scanner;
+
+/**
+ * Count the number of commas (",") in the text received from the console.
+ */
+public class ProgramTask12 {
+    public static void main(String[] args) {
+        int count = 0;
+        Scanner sc = new Scanner(System.in);
+        for (char c : sc.nextLine().toCharArray()) {
+            if (c == ',') {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
